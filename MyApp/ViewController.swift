35afa01b4930c258cddea39d9623516c4683122a
zhixiangtan <tzxdtc10@gmail.com>
2019-12-11 23:46:49 +0900

    add register feature

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var account: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var register: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func registerTouch(_ sender: UIButton) {
+        if let email = account.text, let password = password.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+              // ...
+                print("error",error)
+            }
+            
+        }
+   
+        print("hello world")
+    }
+    
 
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MyApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MyApp
+  pod 'Firebase'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+
+  target 'MyAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MyAppUITests' do
+    # Pods for testing
+  end
+
+end
